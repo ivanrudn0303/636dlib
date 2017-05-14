fdaab8650821f564209a3bf3efa4ed8177aed6a5
nefanov <efanov@phystech.edu>
2017-05-14 14:51:38 +0300

    Workable draft for matrix product implementation

--- matr.c
+++ matr.c
@@ -4,21 +4,22 @@
 
 //Input: a - matrix (dim * dim), b - matrix (dim * dim) 
 //Output: none
-//Stdout: result ('res' matrix);
+//stdout: result ('res' matrix);
 void matr(double **a, double **b, int dim)
 {
 	//put your source code here
 	//
+	double **res;
 	int i = 0;
 	int j = 0;
-	printf("It works!\n");
-/*
+
+
 	for(;i<dim;i++)
 	{
 		for (;j<dim;j++)
 			printf("%lf ", res[i][j]);
 		printf("\n");
 	}
-*/
+
 	return;
 }
